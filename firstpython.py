d712f3479c2a743c66e4c7e4ca39c08a9f632795
parthpatel2374 <dcpatel577@gmail.com>
2023-03-22 23:39:58 +0530

    practiced on firstpython and renamed firstjava

---
+++ firstpython.py
@@ -0,0 +1,47 @@
+"""
+cont from python booleans...
+
+a = "Hi I am the \"\n\" #creator"
+print(a)
+px=18
+vx=19
+kx=20
+y="My name is Pth, I am {2} years old."
+"My friend Vnd is {1} years old."
+"And another friend Ktn is {0} years old."
+print(y.format(kx, vx, px))
+print(x.replace('H', 'P'))
+print(x.split(" "))
+print(x.strip())
+print(x.lower())
+print(x.upper())
+print(y[-5: -0])
+print(y[-5:])
+print(y[:-5])
+print(y[4:])
+print(y[:4])
+print(y[0:4])
+print(len(y))
+for y in "Hello Python":
+	print(y)
+x= "Hello Python, how are your learning experience going"
+"so far!I am happy to talk with you again,"
+"hello world is world universal favourite tags one of the"
+"i mean!"
+y='Hello Python'
+print(x)
+print(y)
+x=int(5.25)
+print(x)
+x=5
+y="John"
+print(x, y)
+x="Python "
+y="is "
+z="awesome."
+print(x+y+z)
+if(5>2):
+	print("hi world")
+this print is commented!!!
+print("Hello, World!")
+"""
